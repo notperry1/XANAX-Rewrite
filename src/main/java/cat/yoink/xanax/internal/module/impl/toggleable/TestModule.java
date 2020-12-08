e7d04a95691fde269b6fd14d450c2b41bb3a5874
Katatje <geertspruijt6@outlook.com>
2020-12-09 01:06:54 +0800

    Add a setting in the test module

--- src/main/java/cat/yoink/xanax/internal/module/impl/toggleable/TestModule.java
+++ src/main/java/cat/yoink/xanax/internal/module/impl/toggleable/TestModule.java
@@ -3,11 +3,14 @@ package cat.yoink.xanax.internal.module.impl.toggleable;
 import cat.yoink.xanax.internal.module.ModuleCategory;
 import cat.yoink.xanax.internal.module.main.ModuleData;
 import cat.yoink.xanax.internal.module.stage.StateModule;
+import cat.yoink.xanax.internal.setting.types.StateSetting;
 import org.lwjgl.input.Keyboard;
 
 @ModuleData(name = "Test", category = ModuleCategory.MISC, defaultBind = Keyboard.KEY_R)
 public final class TestModule extends StateModule
 {
+    private final StateSetting test = addSetting(new StateSetting("Test", true));
+
     @Override
     public void onEnable()
     {
